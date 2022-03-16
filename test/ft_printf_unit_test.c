041168cc023a2573b50ae49d9fbf85db45cb6d21
jniemine <jniemine@c1r3p5.hive.fi>
2022-03-16 21:12:34 +0200

    bk

--- test/ft_printf_unit_test.c
+++ test/ft_printf_unit_test.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <wait.h>
+#include <limits.h>
 
 /*	%[$][flags][width][.precision][length modifier]conversion
  *
@@ -17,43 +18,6 @@ void setUp(void) {
 
 void tearDown(void) {
     // clean stuff up here
-}
-
-void test_plus_flag(void) {
-	char *test_name = "test_plus_flag";
-	char res[100];
-	char res2[100];
-	int p[2], p2[2], stdout_bk;
-
-	bzero(res, 100);
-	bzero(res2, 100);
-	if (pipe(p) < 0)
-		exit(-1);
-	stdout_bk = dup(fileno(stdout));
-	dup2(p[1], fileno(stdout)); 
-	printf("Integers: %+d and %+d and %+d\tFloats: %+f and %+f and %+f\n", 5, -5, 0, 5.678, -5.678, 0);  
-	fflush(stdout);
-	close(p[1]);
-	if (pipe(p2) < 0)
-		exit(-1);
-	dup2(p2[1], fileno(stdout)); 
-	printf("Integers: %+d and %+d and %+d\tFloats: %+f and %+f and %+f\n", 5, -5, 0, 5.678, -5.678, 0);  
-	fflush(stdout);
-	close(p2[1]);
-	dup2(stdout_bk, fileno(stdout));
-	read(p[0], res, 100);
-	read(p2[0], res2, 100);
-	if (strcmp(res, res2) != 0)
-	{
-		printf("%s:\n", test_name);
-		printf("Expected: %s", res);
-		printf("Was:	  %s", res2);
-	}else{
-		printf("OK\n");
-	}
-	close (p[0]);
-	close (p2[0]);
-	return ;
 }
 
 void test_hash_flag(void)
@@ -197,7 +161,7 @@ void test_space_flag(void)
 	if (pipe(p2) < 0)
 		exit(-1);
 	dup2(p2[1], fileno(stdout)); 
-	printf("With - flag: |%-4d|, |%-4i|, |%-4o|, |%-4u|, |%-4x|, |%-4X|, |%-8.1f|\n", 42, 42, 42, 42, 42, 42, 42.0); 
+	printf("With ' ' flag: |% d|, |% d|, |% i|, |% o|, |% u|, |% x|, |% X|, |% f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0); 
 	fflush(stdout);
 	close(p2[1]);
 	dup2(stdout_bk, fileno(stdout));
@@ -216,35 +180,179 @@ void test_space_flag(void)
 	return ;
 }
 
-void addition_for_pipe(void)
-{
-	printf("When this gets printed??\n");
+void test_plus_flag(void)
+{	
+	char *test_name = "test_plus_flag";
+	char res[200];
+	char res2[200];
+	int p[2], p2[2], stdout_bk;
+
+	bzero(res, 200);
+	bzero(res2, 200);
+	if (pipe(p) < 0)
+		exit(-1);
+	stdout_bk = dup(fileno(stdout));
+	dup2(p[1], fileno(stdout)); 
+	//Does nothing with ouxX because those are unsigned conversions not signed!
+	printf("With + flag: |%+d|, |%+d|, |%+i|, |%+o|, |%+u|, |%+x|, |%+X|, |%+f|, |%+f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	fflush(stdout);
+	close(p[1]);
+	if (pipe(p2) < 0)
+		exit(-1);
+	dup2(p2[1], fileno(stdout)); 
+	printf("With ' ' flag: |% d|, |% d|, |% i|, |% o|, |% u|, |% x|, |% X|, |% f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0); 
+	fflush(stdout);
+	close(p2[1]);
+	dup2(stdout_bk, fileno(stdout));
+	read(p[0], res, 200);
+	read(p2[0], res2, 200);
+	if (strcmp(res, res2) != 0)
+	{
+		printf("%s:\n", test_name);
+		printf("Expected:\n%s\n", res);
+		printf("Was:\n%s", res2);
+	}else{
+		printf("OK\n");
+	}
+	close (p[0]);
+	close (p2[0]);
+	return ;
 }
 
-void pipe_test(void){
-	char res[100] = {'\0'};
-	int p[2], stdout_bk;
-//	pid_t pid;
+void test_min_width(void)
+{	
+	char *test_name = "test_min_width";
+	char res[200];
+	char res2[200];
+	int p[2], p2[2], stdout_bk;
 
-//	pid = fork();
-	printf("Before building the pipe\n");
-	stdout_bk = dup(fileno(stdout));
+	bzero(res, 200);
+	bzero(res2, 200);
 	if (pipe(p) < 0)
-		return ;
-	while(dup2(p[1], 1) == -1) {};
-	printf("This is from pipe\n");
+		exit(-1);
+	stdout_bk = dup(fileno(stdout));
+	dup2(p[1], fileno(stdout)); 
+	printf("Width test: |%6d|, |%6d|, |%6i|, |%6o|, |%6u|, |%6x|, |%6X|, |%8.2f|, |%8.2f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	printf("Truncation: |%0d|, |%1d|, |%1i|, |%1o|, |%1u|, |%1x|, |%1X|, |%1f|, |%0f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
 	fflush(stdout);
-	addition_for_pipe();
-	write(1, "this is second thing from pipe", 30);
 	close(p[1]);
-
-	dup2(stdout_bk, 1);
-	printf("We have demolished the pipeline\n");
-	read(p[0], res, 100);
-	printf("%s", res);
-	close(p[0]);
+	if (pipe(p2) < 0)
+		exit(-1);
+	dup2(p2[1], fileno(stdout)); 
+	printf("Width test: |%6d|, |%6d|, |%6i|, |%6o|, |%6u|, |%6x|, |%6X|, |%8.2f|, |%8.2f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	printf("Truncation: |%0d|, |%1d|, |%1i|, |%1o|, |%1u|, |%1x|, |%1X|, |%1f|, |%0f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	fflush(stdout);
+	close(p2[1]);
+	dup2(stdout_bk, fileno(stdout));
+	read(p[0], res, 200);
+	read(p2[0], res2, 200);
+	if (strcmp(res, res2) != 0)
+	{
+		printf("%s:\n", test_name);
+		printf("Expected:\n%s\n", res);
+		printf("Was:\n%s", res2);
+	}else{
+		printf("OK\n");
+	}
+	close (p[0]);
+	close (p2[0]);
 	return ;
-}	
+}
+
+void test_precision(void)
+{	
+	char *test_name = "test_precision";
+	char res[200];
+	char res2[200];
+	int p[2], p2[2], stdout_bk;
+
+	bzero(res, 200);
+	bzero(res2, 200);
+	if (pipe(p) < 0)
+		exit(-1);
+	stdout_bk = dup(fileno(stdout));
+	dup2(p[1], fileno(stdout)); 
+	printf("precision test: |%.4d|, |%.4d|, |%.4i|, |%.4o|, |%.4u|, |%.4x|, |%.4X|, |%.2f|, |%.2f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	printf("for string    : |%.s|, |%.3s|, |%.10s|\n", "Wisdom", "Wisdom", "Wisdom"); 
+	printf("With just '.' : |%.d|, |%.d|, |%.i|, |%.o|, |%.u|, |%.x|, |%.X|, |%.f|, |%.f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	fflush(stdout);
+	close(p[1]);
+	if (pipe(p2) < 0)
+		exit(-1);
+	dup2(p2[1], fileno(stdout)); 
+	printf("precision test: |%.4d|, |%.4d|, |%.4i|, |%.4o|, |%.4u|, |%.4x|, |%.4X|, |%.2f|, |%.2f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	printf("for string    : |%.s|, |%.3s|, |%.10s|\n", "Wisdom", "Wisdom", "Wisdom"); 
+	printf("With just '.' : |%.d|, |%.d|, |%.i|, |%.o|, |%.u|, |%.x|, |%.X|, |%.f|, |%.f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	fflush(stdout);
+	close(p2[1]);
+	dup2(stdout_bk, fileno(stdout));
+	read(p[0], res, 200);
+	read(p2[0], res2, 200);
+	if (strcmp(res, res2) != 0)
+	{
+		printf("%s:\n", test_name);
+		printf("Expected:\n%s\n", res);
+		printf("Was:\n%s", res2);
+	}else{
+		printf("OK\n");
+	}
+	close (p[0]);
+	close (p2[0]);
+	return ;
+}
+
+void test_length_mod_hh(void)
+{	
+	char *test_name = "test_length_mod_hh";
+	char res[500];
+	char res2[500];
+	int p[2], p2[2], stdout_bk;
+
+	bzero(res, 200);
+	bzero(res2, 200);
+	if (pipe(p) < 0)
+		exit(-1);
+	stdout_bk = dup(fileno(stdout));
+	dup2(p[1], fileno(stdout)); 
+	//When integer is given as argument, output is the value which corresponds to conversion. So hh does nothing.
+	//With overflow and underflow, trust casting, it causes UB. With overflows then output is argument mod 128, sometimes in decimal form.
+	printf("length mod hh: |%hhd|, |%hhd|, |%hhi|, |%hho|, |%hhu|, |%hhx|, |%hhX|, |%hhf|, |%hhf|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	signed char c = -1;
+	//with negative value, just the value gets printed. 
+	printf("length mod hh: |%hhd|, |%hhd|, |%hhi|, |%hho|, |%hhu|, |%hhx|, |%hhX|, |%hhf|\n", c, c, c, c, c, c, c, c, c); 
+	unsigned char ch = 'c';
+	//float prints just zeroes, assumably undefined behavior.
+	printf("length mod hh: |%hhd|, |%hhd|, |%hhi|, |%hho|, |%hhu|, |%hhx|, |%hhX|, |%hhf|\n", ch, ch, ch, ch, ch, ch, ch, ch, ch); 
+	ch = 0;
+	printf("length mod hh: |%hhd|, |%hhd|, |%hhi|, |%hho|, |%hhu|, |%hhx|, |%hhX|, |%hhf|\n", ch, ch, ch, ch, ch, ch, ch, ch, ch); 
+	fflush(stdout);
+	fflush(stdout);
+	close(p[1]);
+	if (pipe(p2) < 0)
+		exit(-1);
+	dup2(p2[1], fileno(stdout)); 
+	printf("precision test: |%.4d|, |%.4d|, |%.4i|, |%.4o|, |%.4u|, |%.4x|, |%.4X|, |%.2f|, |%.2f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	printf("for string    : |%.s|, |%.3s|, |%.10s|\n", "Wisdom", "Wisdom", "Wisdom"); 
+	printf("With just '.' : |%.d|, |%.d|, |%.i|, |%.o|, |%.u|, |%.x|, |%.X|, |%.f|, |%.f|\n", 42, -42, 42, 42, 42, 42, 42, 42.0, -42.0); 
+	fflush(stdout);
+	close(p2[1]);
+	dup2(stdout_bk, fileno(stdout));
+	read(p[0], res, 500);
+	read(p2[0], res2, 500);
+	if (strcmp(res, res2) != 0)
+	{
+		printf("%s:\n", test_name);
+		printf("Expected:\n%s\n", res);
+		printf("Was:\n%s", res2);
+	}else{
+		printf("OK\n");
+	}
+	close (p[0]);
+	close (p2[0]);
+	return ;
+}
+
+
 // not needed when using generate_test_runner.rb
 int main(void) {
 //    UNITY_BEGIN();
@@ -253,8 +361,10 @@ int main(void) {
 //	test_hash_flag();
 //	test_zero_flag();
 // 	test_minus_flag();
-	test_space_flag();
-//	pipe_test();
+//	test_space_flag();
+//	test_min_width();
+//	test_precision();
+	test_length_mod_hh();
 	return (0);
 //    return UNITY_END();
 }
--- test_at_school/investigate_float_print.c
+++ test_at_school/investigate_float_print.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main(void)
 {
-	printf("%f\n", 42);  
+	char c = -1;
+	printf("%hhd\n", c);
+	int d = c;
+	printf("%hhd\n", d);
+	double f = d;
+	printf("%hhf\n", f);
 	printf("With hash: %#o and %#x and %#X and %#f and %#.0f and %#f\n", 42, 42, 42, 42, 42, 42);  
 	printf("With hash: %#o and %#x and %#X and %#f and %#.0f and %#f\n", 42, 42, 42, 42, 42, 42.0);  
 	printf("With hash: %#o and %#x and %#X and %#f and %#.0f and %#f\n", 42, 42, 42, 42, 42, (float)42);  
